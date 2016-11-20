cc76d6b654e765bf91a9f7dd78e2d9f775cdb50d
Richard King <richking@scholar.cc>
2016-11-21 10:48:12 +1100

    Update gorilla.py
    debug global vars and clean up

--- gorilla.py
+++ gorilla.py
@@ -73,82 +73,23 @@ def diffInst_func():
     
 # Capture IoT button clickType event
 def lambda_handler(event, context):
+    global ec2
     clickType = event['clickType']
     print("Received event: " + json.dumps(event, indent=2))
-    
-    ''' Replaced by az_func() function
-    # randomize availability zone
-    region="ap-southeast-2" ### <-- Edit this for your target AWS region
-    az=random.choice(string.ascii_letters[0:3]) ### <-- Edit [0:n] where '' is number of AZ's in your region
-    randAz=region+az
-    print("Target Region: ",region)
-    print("Target Availability Zone: ",randAz)
-    '''
-    
+        
     # call function to randomize AZ
     az_func()
     
     ec2 = boto3.resource('ec2')
     
+    # call function for all instances
     inst_func()
-    ''' replaced by func_instIds
-    # add all deployed instances in randAz to list instIds
-    instances = ec2.instances.filter(
-        Filters=[
-            {
-                'Name': 'availability-zone',
-                'Values': [randAz]
-            },
-            {
-                'Name': 'instance-state-name',
-                'Values': ['pending','running','shutting-down','stopping','stopped']
-            },
-        ],
-    )
-    instIds = []
-    for instance in instances:
-        instIds.append(instance.id)
-    print ("\nAll instances in", randAz)
-    print ("\n".join(instIds))
-    '''
     
+    # call function for ASG instances
     asgInst_func()
-    '''
-    # add only ASG deployed instances in randAz into list asgInstIds
-    # any instance deployed with an ASG is tagged with Key='tag:aws:autoscaling:groupName', Value='<ASG Name>'
-    # we capture this by outputting all instances in our target AZ and filtering on the aboce Key with a wildcard Value.
-    # we also only want to instances in a steady or 'running' state.
-    asgInstances = ec2.instances.filter(
-        Filters=[
-            {
-                'Name': 'availability-zone',
-                'Values': [randAz]
-            },
-            {
-                'Name': 'tag:aws:autoscaling:groupName',
-                'Values': ['*']
-            },
-            {
-                'Name': 'instance-state-name',
-                'Values': ['running']
-            },
-        ],
-    )
-    asgInstIds = []
-    for instance in asgInstances:
-        asgInstIds.append(instance.id)
-    print ("\nASG deployed instances targeted for termination in", randAz)
-    print ("\n".join(asgInstIds))
-    '''
     
+    # call function to diff instance lists
     diffInst_func()
-    '''
-    # Now diff the lists using set() and print output 
-    nonAsg=(set(instIds)-set(asgInstIds))
-    print ("\nInstances safe from the gorilla.")
-    print ("These instances have _not_ been deployed with autoscale groups and services may not auto-heal. You should do something about that...")
-    print ("\n".join(nonAsg))
-    '''
     
     '''
     Here comes the big bad stuff that terminates instances in asgInstIds
